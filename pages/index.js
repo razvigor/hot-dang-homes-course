293e6dbc248445af2c2dd7c672537e379228a37d
razvigor <kamaradsasa@gmail.com>
2022-10-15 14:21:58 +0200

    dodatak

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      exampleprop: "test",
+    },
+  };
+};
